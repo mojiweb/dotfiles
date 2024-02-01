b0319bd591e0300d48f9582452c9bed52a70a3c4
Moji <mojiweb@gmail.com>
2024-02-01 21:32:34 +0100

    Reorganize lua files

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,27 @@
-  return {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
+return {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  config = function()
+    require("lualine").setup({
       options = {
+        -- theme = "catppuccin",
         theme = 'onedark',
+        globalstatus = true,
       },
-    },
-  }
+      sections = {
+        lualine_b = {
+          "branch",
+          "diff",
+          "diagnostics",
+        },
+        lualine_c = {
+          { "filename", path = 1 },
+        },
+        lualine_x = {
+          "filetype",
+        },
+      },
+    })
+  end,
+}
